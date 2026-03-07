c4e25fbb7ffe730062b8f53fd0a9b7030c28962e
masud2005 <masud20012005@gmail.com>
2026-03-07 15:52:07 +0600

    app module file imports throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerConfigModule } from './common/modules/throttler.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerConfigModule,
     PrismaModule,
   ],
   controllers: [AppController],
