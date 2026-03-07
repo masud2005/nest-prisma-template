c565195494d0c4f51751800f0606879759d07600
masud2005 <masud20012005@gmail.com>
2026-03-07 12:28:51 +0600

    swagger add

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Global prefix
+  app.setGlobalPrefix('api/v1');
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Backend API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: false,
+  });
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  const PORT = process.env.PORT ?? 3000;
+  await app.listen(PORT, '0.0.0.0');
+  console.log(`Server running on PORT ${PORT}`);
+  console.log(`Local access:   http://localhost:${PORT}/api/v1`);
+  console.log(`Network access: http://10.10.10.32:${PORT}/api/v1`);
 }
+
 bootstrap();
